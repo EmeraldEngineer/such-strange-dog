a1fb067d09fdd3773e18020b6ad93855cf20c2ff
Taylor McCarthy <oresshi@gmail.com>
2017-01-23 00:50:56 -0700

    PWP milestone 1

--- milestone-1.php
+++ milestone-1.php
@@ -1,7 +1,48 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: blue
- * Date: 1/22/17
- * Time: 1:49 PM
- */
+ Taylor McCarthy PWP - Such Strange Dog Training
+ **/
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Such Strange Dog Training Milestone One</title>
+	</head>
+	<body>
+		<header>
+			<h1>Persona and Use Case for Such Strange Dog Training Website</h1>
+		</header>
+		<br>
+		<h2>Purpose</h2>
+		<p>Design a professional, elegant looking website for The Such Strange Dog Training company. This website will both be used to attract clients for Such Strange Dog Training.</h2>
+		<br>
+		<h3>Audience</h3>
+		<p>Potential and existing clients of Such Strange Dog Training.</p>
+		<br>
+		<h4>Goal</h4>
+		<p>To attract new clients for Such Strange Dog Training and help existing clients find contact information and any other resources they may need.</p>
+		<br><br>
+		<h5>Persona</h5>
+		<ul>
+			<li>Name: Jerry Nobork</li>
+			<li>Age: 50</li>
+			<li>Technology: Macbook Pro running OS X El Capitan. iPhone 5 running latest version of iOS.</li>
+			<li>Goals<
+				<ul>
+					<li>Improve the behavior of their dog.</li>
+					<li>Obtain contact information for Such Strange Dog Training</i>
+					<li>Obtain information about pricing and scheduling availability.</li>
+				</ul>
+			</li>
+			<li>Frustrations
+				<ul>
+					<li>Dog is a jerk</li>
+				</ul>
+			</li>
+		</ul>
+		<br>
+		<h6>Use Case</h6>
+		<p>Jerry Nobork is a busy accountant who has recently obtain a new puppy. His puppy has been eliminating in the house and displaying destructive behavior. Jerry needs a dog trainer who will travel to his home outside of the city to help him learn good training habits he can apply in his home. He wants to be able to compare pricing and other info online so he can fit this research inbetween calls to and from his clients.
+		</p>
+	</body>
+</html>
