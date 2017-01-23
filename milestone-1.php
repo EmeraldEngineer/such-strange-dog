b6e2d55ecb6b57bf0f1a190b61c0dadeed560bf8
Taylor McCarthy <oresshi@gmail.com>
2017-01-23 00:54:03 -0700

    Fixed h tags.

--- milestone-1.php
+++ milestone-1.php
@@ -12,16 +12,16 @@
 			<h1>Persona and Use Case for Such Strange Dog Training Website</h1>
 		</header>
 		<br>
-		<h2>Purpose</h2>
+		<h1>Purpose</h1>
 		<p>Design a professional, elegant looking website for The Such Strange Dog Training company. This website will both be used to attract clients for Such Strange Dog Training.</h2>
 		<br>
-		<h3>Audience</h3>
+		<h1>Audience</h1>
 		<p>Potential and existing clients of Such Strange Dog Training.</p>
 		<br>
-		<h4>Goal</h4>
+		<h1>Goal</h1>
 		<p>To attract new clients for Such Strange Dog Training and help existing clients find contact information and any other resources they may need.</p>
 		<br><br>
-		<h5>Persona</h5>
+		<h1>Persona</h1>
 		<ul>
 			<li>Name: Jerry Nobork</li>
 			<li>Age: 50</li>
@@ -40,7 +40,7 @@
 			</li>
 		</ul>
 		<br>
-		<h6>Use Case</h6>
+		<h1>Use Case</h1>
 		<p>Jerry Nobork is a busy accountant who has recently obtain a new puppy. His puppy has been eliminating in the house and displaying destructive behavior. Jerry needs a dog trainer who will travel to his home outside of the city to help him learn good training habits he can apply in his home. He wants to be able to compare pricing and other info online so he can fit this research inbetween calls to and from his clients.
 		</p>
 	</body>
