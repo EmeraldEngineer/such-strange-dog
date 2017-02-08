45c625961a162aef8ae0aab9fb8cd80b22aa0ec8
Taylor McCarthy <oresshi@gmail.com>
2017-02-08 10:59:34 -0700

    created milestone 2 php

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -9,18 +9,20 @@
 		</p>
 			<ul>
 			<li>Overall help new and existing clients find info about pricing, contact info, and other recommended resources.
-			</li>
+				<ul>
 
-			<li>About: Contains basic info about the business history and trainer experience.
-			</li>
+					<li>About: Contains basic info about the business history and trainer experience.
+					</li>
 
-			<li>Pricing: Contains info about pricing and services offered, including any current discounts or other promotions.
-			</li>
+					<li>Pricing: Contains info about pricing and services offered, including any current discounts or other promotions.
+					</li>
 
-			<li>Contact: Contains contact info; phone, email, etc.
-			</li>
+					<li>Contact: Contains contact info; phone, email, etc.
+					</li>
 
-			<li>Resources: Contains links to external sites that may be useful to clients; Humane Society website, useful articles/videos, list of city dog parks, etc.
+					<li>Resources: Contains links to external sites that may be useful to clients; Humane Society website, useful articles/videos, list of city dog parks, etc.
+					</li>
+				</ul>
 			</li>
 			</ul>
 		<br>
