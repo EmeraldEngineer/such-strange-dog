295dfe3c4756865420bd9b501f90de7e6ed37166
Taylor McCarthy <oresshi@gmail.com>
2017-02-08 10:36:04 -0700

    created milestone 2 php

---
+++ documentation/milestone-2.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Such Strange Dog Training Milestone 2</title>
+	</head>
+	<body>
+		<p>Content Strategy
+			<ul>
+			<li>Overall help new and existing clients find info about pricing, contact info, and other recommended resources.
+			</li>
+
+			<li>About: Contains basic info about the business history and trainer experience.
+			</li>
+
+			<li>Pricing: Contains info about pricing and services offered, including any current discounts or other promotions.
+			</li>
+
+			<li>Contact: Contains contact info; phone, email, etc.
+			</li>
+
+			<li>Resources: Contains links to external sites that may be useful to clients; Humane Society website, useful articles/videos, list of city dog parks, etc.
+			</li>
+			</ul>
+		</p>
+	</body>
+</html>
